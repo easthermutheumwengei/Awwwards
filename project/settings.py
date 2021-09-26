9af720d011287e2e0d3234d27ee1c535281ea1cb
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-26 18:08:53 +0300

    make changes to the project

--- project/settings.py
+++ project/settings.py
@@ -26,24 +26,23 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # development
 if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
+    DATABASES = {
+        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
+           'HOST': config('DB_HOST')
+        }
        
-   }
+    }
 # production
 else:
-   DATABASES = {
-       'default': dj_database_url.config(
+    DATABASES = {
+        'default': dj_database_url.config(
            default=config('DATABASE_URL')
-       )
-   }
+        )
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
