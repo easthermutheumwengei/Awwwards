c940d577a09823fa77799d6820fe67e8fe2351fe
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-26 17:40:17 +0300

    modify forms

--- awwwards/forms.py
+++ awwwards/forms.py
@@ -4,24 +4,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import Profile, Project, Review
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.pop("autofocus", None)
+
 
 class NewProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description', 'screenshot', 'repository_url', 'live_url']
 
+
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
